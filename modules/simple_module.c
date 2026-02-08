6f428e303ab3f58f93fab123b38d6d0c8c02e333
Jonathan Rodriguez-Gomez <j.rodriguezgomez@wustl.edu>
2026-02-08 13:18:29 -0600

    add hrtimer boilerplate

--- modules/simple_module.c
+++ modules/simple_module.c
@@ -3,35 +3,49 @@
    Love's "Linux Kernel Development, Third Edition."
  */
 
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/errno.h>
+#include <linux/hrtimer.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/ktime.h>
+#include <linux/module.h>
 
 static unsigned long log_sec = 1;
 static unsigned long log_nsec = 0;
-module_param(log_sec,  ulong, 0);
+module_param(log_sec, ulong, 0);
 module_param(log_nsec, ulong, 0);
 
+ktime_t interval;
+struct hrtimer timer;
+
+enum hrtimer_restart timer_callback(struct hrtimer *timer_ptr) {
+  printk(KERN_ALERT "timer restarted.\n");
+  hrtimer_forward_now(timer_ptr, interval);
+  return HRTIMER_RESTART;
+}
+
 /* init function - logs that initialization happened, returns success */
-static int
-simple_init(void)
-{
-    printk(KERN_ALERT "simple module initialized with error\n");
-    return 0;
+static int simple_init(void) {
+  interval = ktime_set(log_sec, log_nsec);
+  hrtimer_init(&timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+
+  timer.function = &timer_callback;
+  hrtimer_start(&timer, interval, HRTIMER_MODE_REL);
+
+  printk(KERN_ALERT "simple module initialized\n");
+  return 0;
 }
 
 /* exit function - logs that the module is being removed */
-static void
-simple_exit(void)
-{
-    printk(KERN_ALERT "simple module is being unloaded\n");
-    printk(KERN_ALERT "%lu %lu\n", log_sec, log_nsec);
+static void simple_exit(void) {
+  hrtimer_cancel(&timer);
+
+  printk(KERN_ALERT "simple module is being unloaded\n");
 }
 
 module_init(simple_init);
 module_exit(simple_exit);
 
-MODULE_LICENSE ("GPL");
-MODULE_AUTHOR ("CSE 4202 Team");
-MODULE_DESCRIPTION ("Lab 1");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("CSE 4202 Team");
+MODULE_DESCRIPTION("Lab 1");
